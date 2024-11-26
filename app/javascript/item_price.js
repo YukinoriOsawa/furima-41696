ab23bf1a3062c38f4962406e045b2e8278569342
YukinoriOsawa <pen.yuki1211@gmail.com>
2024-11-26 15:55:45 +0900

    価格表示　修正

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,14 +1,15 @@
 const price = () => {
 const priceInput = document.getElementById("item-price");
 priceInput.addEventListener("input", () => {
-  const inputValue = priceInput.value;
-  
- const addTaxDom = document.getElementById("add-tax-price");
-    addTaxDom.innerHTML = Math.floor(inputValue * 0.1);
-    const profitDom = document.getElementById("profit");
-    profitDom.innerHTML = Math.floor(inputValue * 0.9);
-  })
-  };
+  const inputValue = parseInt(priceInput.value, 10) || 0;
 
+  const addTaxDom = document.getElementById("add-tax-price");
+  const tax = Math.floor(inputValue * 0.1); 
+  addTaxDom.innerHTML = tax;
+  const profitDom = document.getElementById("profit");
+  const profit = inputValue - tax;
+  profitDom.innerHTML = profit;
+})
+};
   window.addEventListener("turbo:load", price);
   window.addEventListener("turbo:render", price);
