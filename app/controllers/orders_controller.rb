a5d210955e3abced6126d73a9346a12b230f6f28
YukinoriOsawa <pen.yuki1211@gmail.com>
2024-12-02 12:18:52 +0900

    クレジットカード実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -8,7 +8,9 @@ class OrdersController < ApplicationController
 
   def create
     @purchase_address = PurchaseAddress.new(purchase_params)
-    if @purchase_address.save
+    if @purchase_address.valid?
+      pay_item
+      @purchase_address.save
       redirect_to root_path, notice: '購入が完了しました。'
     else
       flash.now[:alert] = '購入に失敗しました。入力内容を確認してください。'
@@ -26,11 +28,21 @@ class OrdersController < ApplicationController
       :addresses, 
       :building, 
       :phone_number,
-    ).merge(user_id: current_user.id, item_id: @item.id)
+    ).merge(user_id: current_user.id, item_id: @item.id,token: params[:token])
   end
 
   def set_item
+    gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
     @item = Item.find(params[:item_id])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"] 
+    Payjp::Charge.create(
+      amount: @item.price,  # 商品の値段
+      card: purchase_params[:token],    # カードトークン
+      currency: 'jpy'                 # 通貨の種類（日本円）
+    )
   end
 
 end
